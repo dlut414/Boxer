d3406929cfadd420a6fc37da48b4f6535b77c6cf
dlut414 <hfy8373@gmail.com>
2017-11-22 23:46:42 +0900

    writing rival function

--- Boxer.h
+++ Boxer.h
@@ -3,6 +3,7 @@
 ///2017.11~
 #pragma once
 #include <vector>
+#include <algorithm>
 
 template <typename T>
 class Generation{
@@ -13,6 +14,19 @@ public:
 		units = std::vector<T>(num, T());
 	}
 	void rival(){
+		std::random_shuffle(units.begin(), units.end());
+		typedef std::pair<int,int> pii;
+		std::vector<pii> scores_id(num);
+		for(int i=0;i<num-1;i+=2){
+			if(units[i].challenge(units[i+1])){
+				
+			}
+			else{
+				
+			}
+		}
+	}
+	void survival(){
 		
 	}
 	void mutate(){
